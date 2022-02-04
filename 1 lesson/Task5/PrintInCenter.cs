5ab4d89a23ee663e978fda88960e3eb15f43afc0
Dmitriy Safin <safindm@gmail.com>
2022-02-04 14:56:10 +0300

    Первое домашнее задание добавлена 5 задача

---
+++ 1 lesson/Task5/PrintInCenter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class OutputHelpers
+    {
+        public static void PrintInCenter(string text)
+        {
+            Console.SetCursorPosition((Console.WindowWidth - text.Length) / 2, Console.WindowHeight / 2 - 1);
+            Console.WriteLine(text);
+        }
+
+        public static void Pause()
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1 lesson/Task5/Program5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: "); //запрос на ввод данных
+            string nameInput = Console.ReadLine(); //ввод пользователем запрашиваемых данных
+
+            Console.Write("Введите фамилию: ");
+            string surnameInput = Console.ReadLine();
+
+            Console.Write("Введите город проживания: ");
+            string cityInput = Console.ReadLine();
+
+            Console.Clear();
+            OutputHelpers.PrintInCenter($"{ surnameInput} { nameInput}, г. { cityInput}");
+            OutputHelpers.Pause();
+        }
+    }
+}
