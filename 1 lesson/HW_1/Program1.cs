f57c275da8750e281de463227d6c6ae5711512f0
Dmitriy Safin <safindm@gmail.com>
2022-02-04 14:54:31 +0300

    Первое домашнее задание 4 задачи

---
+++ 1 lesson/HW_1/Program1.cs
@@ -0,0 +1,43 @@
+﻿/* Сафин Дмитрий
+ * 1.	Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку:
+    а) используя  склеивание;
+	б) используя форматированный вывод;
+	в) используя вывод со знаком $.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите имя: "); //запрос на ввод данных
+            string Name = Console.ReadLine(); //ввод пользователем запрашиваемых данных
+
+            Console.Write("Введите фамилию: ");
+            string Surname = Console.ReadLine();
+
+            Console.Write("Введите возраст: ");
+            string Age = Console.ReadLine();
+
+            Console.Write("Введите рост: ");
+            string Height = Console.ReadLine();
+
+            Console.Write("Введите вес: ");
+            string Weight = Console.ReadLine();
+
+            Console.WriteLine("Имя: " + Name + "; Фамилия: " + Surname + "; Возраст: " + Age + "; Рост: " + Height + "; Вес: " + Weight); //Вывод данных склейкой
+            Console.WriteLine("Имя: {0}; Фамилия: {1}; Возраст: {2}; Рост: {3}; Вес: {4}", Name, Surname, Age, Height, Weight); //Форматированный вывод данных
+            Console.WriteLine($"Имя: {Name}; Фамилия: {Surname}; Возраст: {Age}; Рост: {Height}; Вес: {Weight}"); //Вывод данных через интерполяцию строк
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1 lesson/Task2/Program2.cs
@@ -0,0 +1,32 @@
+﻿/* Сафин Дмитрий
+ * 2.	Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите вес в килограммах: "); //запрос на ввод данных
+            float m = float.Parse(Console.ReadLine()); //ввод пользователем запрашиваемых данных, конвертация типа данных из строки в число и прсвоение значения  
+
+            Console.Write("Введите рост в метрах: ");
+            float h = float.Parse(Console.ReadLine());
+
+            float I = m / (h * h); //вычисление индекса массы тела
+
+            Console.WriteLine("Индекс массы тела (ИМТ): {0}", I); //Вывод полученного результата в консоль
+            
+            Console.ReadLine();
+        }
+    }
+}
