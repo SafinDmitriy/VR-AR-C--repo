5539dd40f5ea5b2f148c1b60e681e64895595d23
Dmitriy Safin <safindm@gmail.com>
2022-02-07 17:09:42 +0300

    2 задача

--- 3 lesson/HW_3/Task2/Program2.cs
+++ 3 lesson/HW_3/Task2/Program2.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* Сафин Дмитрий
+ * 2. а) С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке). 
+ * Требуется подсчитать сумму всех нечётных положительных чисел. 
+ * Сами числа и сумму вывести на экран, используя tryParse.*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +12,43 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    internal class Program
+    internal class Program2
     {
+
+        static int Input()
+        {
+            while (true)
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                    Console.Write("Вы ввели не число. Пожалуйста, повторите ввод: ");
+                else return x;
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Вводите числа, каждое с новой строки, для завершения введите 0: ");
+
+            
+            int sum = 0;    
+            while (true)
+            {
+                int number = Input();
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number > 0 && number % 2 != 0)
+                {
+                    Console.WriteLine("Вами введены следующие нечетные положительные числа: " + number +", ");
+                    sum+=number;
+                }
+            }
+
+            Console.WriteLine("Сумма нечетных положительных чисел равна: " + sum);
+
+            Console.ReadKey();
         }
+
     }
-}
+    }
+
